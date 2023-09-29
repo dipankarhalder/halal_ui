46db0a4636a6ecd8ade5858bb78d53379783bd18
dipankarhalder <webmail.dip@gmail.com>
2023-09-30 02:07:58 +0530

    initlal commits

---
+++ utils/common/staticInfo.ts
@@ -0,0 +1,31 @@
+export const allStaticInfo = {
+  themes: {
+    project_title: "Halal",
+    decspt: "Halal - New user interface dashboard",
+  },
+  common: {
+    wait: "Please wait...",
+  },
+  inputs_text: {
+    btn_submit: "Submit",
+  },
+  auth: {
+    page_heading: "Login to your account",
+    page_desc: "Login to your account so you can continue building and editing your onboarding flows."
+  },
+  context: {
+    error_text: "must be used within a",
+  }
+}
+
+
+
+// import Image from 'next/image'
+// <Image
+//   src="/vercel.svg"
+//   alt="Vercel Logo"
+//   className={styles.vercelLogo}
+//   width={100}
+//   height={24}
+//   priority
+// />
---
+++ utils/interface/index.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from "react";
+
+// core interfaces
+export interface IPageChildProps {
+  children: ReactNode;
+}
